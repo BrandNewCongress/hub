97fdafccc2916beccac1a238a534a79f615d5685
saikat <saikat@gomockingbird.com>
2017-01-04 11:40:14 -0500

    Fix uncaught exception handling

--- src/wrap.js
+++ src/wrap.js
@@ -1,11 +1,9 @@
 /* This is a function wrapper to correctly
  catch and handle uncaught exceptions in
  asynchronous code. */
-import log from './log'
 export default (fn) =>
   (...args) =>
     fn(...args)
       .catch((ex) => {
-        log.error(ex)
         process.nextTick(() => { throw ex })
       })
