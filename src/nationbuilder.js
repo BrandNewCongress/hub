ca1cbbbe9be02e76f03c81e89f8aeec8798161a1
saikat <saikat@gomockingbird.com>
2017-01-22 20:13:00 -0500

    Handle adding new volunteers

--- src/nationbuilder.js
+++ src/nationbuilder.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 import log from './log'
+import url from 'url'
+import { formatLink, formatDistrictCode } from './lib'
 
 class Nationbuilder {
   async makeRequest(method, path, body) {
@@ -16,14 +18,26 @@ class Nationbuilder {
 
     return response
   }
+
+  async createVolunteer(personInfo) {
+    const person = await this.createPerson(personInfo)
+
+  }
+
   async createPerson({
     name,
+    profile,
     email,
     phone,
     address,
+    facebook,
+    twitter,
+    linkedIn,
+    congressionalDistrict,
     utmSource,
     utmMedium,
-    utmCampaign
+    utmCampaign,
+    tags
   }) {
     let nameParts = null
     let firstName = null
@@ -35,33 +49,78 @@ class Nationbuilder {
       firstName = nameParts.shift()
       lastName = nameParts.join(' ')
     }
-    const requestBody = {
-      person: {
-        phone,
-        first_name: firstName,
-        last_name: lastName,
-        email1: email,
-        mailing_address: address
-      }
+    const facebookURL = formatLink(facebook)
+    const twitterURL = formatLink(twitter)
+    const linkedInURL = formatLink(linkedIn)
+    let twitterName = null
+    let linkedInID = null
+    if (twitterURL) {
+      twitterName = url.parse(twitterURL).pathname.split('/')[1]
     }
-    const response = await this.makeRequest('POST', 'people', requestBody)
+    if (linkedInURL) {
+      linkedInID = url.parse(linkedInURL).pathname.split('/')[2]
+    }
+    const utmParameters = {
+      utm_source: utmSource,
+      utm_medium: utmMedium,
+      utm_campaign: utmCampaign
+    }
 
-    if (response && (response.status === 201 || response.status === 409)) {
+    const requestBody = {
+      phone,
+      facebook_profile_url: facebookURL,
+      twitter_login: twitterName,
+      linkedin_id: linkedInID,
+      first_name: firstName,
+      last_name: lastName,
+      email1: email,
+      mailing_address: address,
+      federal_district: formatDistrictCode(congressionalDistrict)
+    }
+    const response = await this.makeRequest('POST', 'people', { person: requestBody })
+    if (response && response.status === 409) {
       const personId = response.data.person.id
-      await this.updatePerson(personId, {
+      const personProfile = response.data.person.note
+      if (tags) {
+        await this.addTagsToPerson(personId, tags)
+      }
+      const updateResponse = await this.updatePerson(personId, {
         person: {
-          utm_source: utmSource,
-          utm_medium: utmMedium,
-          utm_campaign: utmCampaign
+          note: `${personProfile}; ${profile}`,
+          ...requestBody,
+          ...utmParameters
         }
       })
-    } else {
-      throw new Error(`Nationbuilder create person failed with status: ${response ? response.status : 'No response status received'}`)
+
+      return updateResponse
     }
-    return response
+    if (response && response.status === 201) {
+      const personId = response.data.person.id
+      const personProfile = response.data.person.note
+      if (tags) {
+        await this.addTagsToPerson(personId, tags)
+      }
+      const updateResponse = await this.updatePerson(personId, {
+        person: {
+          note: `${personProfile}; ${profile}`,
+          ...utmParameters
+        }
+      })
+      return updateResponse
+    }
+    throw new Error(`Nationbuilder create person failed with status: ${response ? response.status : 'No response status received'}`)
   }
+
   async updatePerson(id, fields) {
     return this.makeRequest('PUT', `people/${id}`, fields)
+  }
+
+  async addTagsToPerson(id, tags) {
+    return this.makeRequest('PUT', `people/${id}/taggings`, {
+      tagging: {
+        tag: tags
+      }
+    })
   }
 }
 
