7730dcf298dc1bbb048921ae99b6a09519c07c0f
saikat <saikat@gomockingbird.com>
2017-01-23 22:00:11 -0500

    Try/catch everything, no more wrap

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import axios from 'axios'
 import bodyParser from 'body-parser'
 import express from 'express'
 import log from './log'
-import wrap from './wrap'
 import mail from './mail'
 import maestro from './maestro'
 import airtable from './airtable'
@@ -33,7 +32,7 @@ async function saveKueJob(job) {
     job.save((err) => {
       if (err) {
         log.error(err)
-        resolve()
+        reject(err)
       }
       resolve()
     })
@@ -51,73 +50,91 @@ app.use((req, res, next) => {
   return next()
 })
 app.use('/queue', auth('admin', process.env.QUEUE_PASSWORD), kue.app)
-app.get('/teams', wrap(async (req, res) => {
-  let teams = await airtable.findAll('Teams')
-  teams = teams.map((team) => ({
-    name: team.get('Name')
-  }))
-  res.send(JSON.stringify(teams))
-}))
-
-app.post('/nominations', wrap(async (req, res) => {
-  const body = req.body
-  if (!body.nominatorName || !body.nominatorEmail || !body.nominatorPhone || !body.nomineeName) {
-    res.sendStatus(400)
-    return
+app.get('/teams', async (req, res) => {
+  try {
+    let teams = await airtable.findAll('Teams')
+    teams = teams.map((team) => ({
+      name: team.get('Name')
+    }))
+    res.send(JSON.stringify(teams))
+  } catch (ex) {
+    log.error(ex)
+    if (req.body.redirect) {
+      res.redirect(req.body.redirect)
+    } else {
+      res.sendStatus(200)
+    }
   }
+})
 
-  const createJob = queue.create('createPerson', {
-    name: body.nominatorName,
-    email: body.nominatorEmail,
-    phone: body.nominatorPhone,
-    utmSource: body.utmSource,
-    utmMedium: body.utmMedium,
-    utmCampaign: body.utmCampaign
-  })
-  await saveKueJob(createJob.attempts(3))
+app.post('/nominations', async (req, res) => {
+  try {
+    const body = req.body
+    if (!body.nominatorName || !body.nominatorEmail || !body.nominatorPhone || !body.nomineeName) {
+      res.sendStatus(400)
+      return
+    }
 
-  const nominationJob = queue.create('createNomination', {
-    'Nominator Name': body.nominatorName,
-    'Nominator Email': body.nominatorEmail,
-    'Nominator Phone': body.nominatorPhone,
-    Name: body.nomineeName,
-    Email: body.nomineeEmail,
-    Phone: body.nomineePhone,
-    City: body.nomineeCity,
-    'State Abbreviation': body.nomineeState,
-    'Congressional District Code': body.nomineeDistrict,
-    Facebook: body.nomineeFacebook,
-    LinkedIn: body.nomineeLinkedIn,
-    Twitter: body.nomineeTwitter,
-    'Relationship to Nominator': body.relationship,
-    Leadership: body.leadership,
-    'Work History': body.work,
-    'Public Speaking': body.publicSpeaking,
-    'Political Views': body.politicalViews,
-    'Political Party': body.politicalParty,
-    'Run for Office': body.runForOffice,
-    'Office Run Results': body.officeRunResults,
-    'Other Info': body.otherInfo,
-    'District Info': body.districtInfo,
-    Source: isEmpty(body.source) ? 'BNC Website' : body.source,
-    'Source Details': body.sourceDetails,
-    'Source Team Name': body.sourceTeamName,
-    'Submitter Email': body.submitterEmail,
-    Profile: body.nomineeProfile,
-    'Other Links': body.nomineeLinks,
-    'UTM Source': body.utmSource,
-    'UTM Medium': body.utmMedium,
-    'UTM Campaign': body.utmCampaign
-  })
+    const createJob = queue.create('createPerson', {
+      name: body.nominatorName,
+      email: body.nominatorEmail,
+      phone: body.nominatorPhone,
+      utmSource: body.utmSource,
+      utmMedium: body.utmMedium,
+      utmCampaign: body.utmCampaign
+    })
+    await saveKueJob(createJob.attempts(3))
 
-  await saveKueJob(nominationJob.attempts(3).backoff(true))
+    const nominationJob = queue.create('createNomination', {
+      'Nominator Name': body.nominatorName,
+      'Nominator Email': body.nominatorEmail,
+      'Nominator Phone': body.nominatorPhone,
+      Name: body.nomineeName,
+      Email: body.nomineeEmail,
+      Phone: body.nomineePhone,
+      City: body.nomineeCity,
+      'State Abbreviation': body.nomineeState,
+      'Congressional District Code': body.nomineeDistrict,
+      Facebook: body.nomineeFacebook,
+      LinkedIn: body.nomineeLinkedIn,
+      Twitter: body.nomineeTwitter,
+      'Relationship to Nominator': body.relationship,
+      Leadership: body.leadership,
+      'Work History': body.work,
+      'Public Speaking': body.publicSpeaking,
+      'Political Views': body.politicalViews,
+      'Political Party': body.politicalParty,
+      'Run for Office': body.runForOffice,
+      'Office Run Results': body.officeRunResults,
+      'Other Info': body.otherInfo,
+      'District Info': body.districtInfo,
+      Source: isEmpty(body.source) ? 'BNC Website' : body.source,
+      'Source Details': body.sourceDetails,
+      'Source Team Name': body.sourceTeamName,
+      'Submitter Email': body.submitterEmail,
+      Profile: body.nomineeProfile,
+      'Other Links': body.nomineeLinks,
+      'UTM Source': body.utmSource,
+      'UTM Medium': body.utmMedium,
+      'UTM Campaign': body.utmCampaign
+    })
 
-  if (body.redirect) {
-    res.redirect(body.redirect)
-  } else {
-    res.sendStatus(200)
+    await saveKueJob(nominationJob.attempts(3).backoff(true))
+
+    if (body.redirect) {
+      res.redirect(body.redirect)
+    } else {
+      res.sendStatus(200)
+    }
+  } catch (ex) {
+    log.error(ex)
+    if (req.body.redirect) {
+      res.redirect(req.body.redirect)
+    } else {
+      res.sendStatus(200)
+    }
   }
-}))
+})
 
 app.post('/people', async (req, res) => {
   try {
@@ -155,79 +172,101 @@ app.post('/people', async (req, res) => {
   }
 })
 
-app.post('/volunteers', wrap(async (req, res) => {
-  const body = req.body
-  const addressLines = body.volunteerAddress ? body.volunteerAddress.split('\n') : []
-  const address = {
-    city: body.volunteerCity,
-    state: body.volunteerState,
-    zip: body.volunteerZip
-  }
-  let counter = 1
-  addressLines.forEach((line) => {
-    if (counter > 3) {
-      return
+app.post('/volunteers', async (req, res) => {
+  try {
+    const body = req.body
+    const addressLines = body.volunteerAddress ? body.volunteerAddress.split('\n') : []
+    const address = {
+      city: body.volunteerCity,
+      state: body.volunteerState,
+      zip: body.volunteerZip
     }
-    address[`address${counter}`] = line
-    counter = counter + 1
-  })
-  const volunteerJob = queue.createJob('createPerson', {
-    name: body.volunteerName,
-    email: body.volunteerEmail,
-    phone: body.volunteerPhone,
-    address: {
-      ...address
-    },
-    linkedIn: body.volunteerLinkedIn,
-    profile: body.volunteerProfile,
-    tags: body.volunteerSkills
-  })
-  await saveKueJob(volunteerJob)
-  if (body.redirect) {
-    res.redirect(body.redirect)
-  } else {
-    res.sendStatus(200)
-  }
-}))
-
-app.get('/people/count', wrap(async (req, res) => {
-  let response = null
-  response = await axios.get(`https://${process.env.NATIONBUILDER_SLUG}.nationbuilder.com/api/v1/people/count?access_token=${process.env.NATIONBUILDER_TOKEN}`, { headers: { Accept: 'application/json', 'Content-Type': 'application/json' }, validateStatus: () => true })
-  if (response) {
-    res.send({ count: response.data.people_count })
-  } else {
-    res.sendStatus(400)
-  }
-}))
-
-// get upcoming conferences REST call
-// path/maestro/upcomingConferences
-// Usage:
-//   name : filter for conferences that contain value of 'name"
-app.get('/conference-calls/upcoming', wrap(async (request, response) => {
-  const name = (typeof request.query.name === 'undefined') ? '' : unescape(request.query.name)
-
-  const upcomingConferences = {
-    conferences: []
-  }
-
-  const upcomingConferenceIds = await maestro.getUpcomingConferences(name)
-  for (let index = 0; index < upcomingConferenceIds.length; index++) {
-    const conferenceData = await maestro.getConferenceData(upcomingConferenceIds[index])
-    const newMaestroConference = maestro.formattedData(conferenceData)
-    upcomingConferences.conferences.push(newMaestroConference)
-  }
-  upcomingConferences.conferences.sort((a, b) => {
-    if (a.timeInSeconds < b.timeInSeconds) {
-      return -1
+    let counter = 1
+    addressLines.forEach((line) => {
+      if (counter > 3) {
+        return
+      }
+      address[`address${counter}`] = line
+      counter = counter + 1
+    })
+    const volunteerJob = queue.createJob('createPerson', {
+      name: body.volunteerName,
+      email: body.volunteerEmail,
+      phone: body.volunteerPhone,
+      address: {
+        ...address
+      },
+      linkedIn: body.volunteerLinkedIn,
+      profile: body.volunteerProfile,
+      tags: body.volunteerSkills
+    })
+    await saveKueJob(volunteerJob)
+    if (body.redirect) {
+      res.redirect(body.redirect)
+    } else {
+      res.sendStatus(200)
     }
-    if (a.timeInSeconds > b.timeInSeconds) {
-      return 1
+  } catch (ex) {
+    log.error(ex)
+    if (req.body.redirect) {
+      res.redirect(req.body.redirect)
+    } else {
+      res.sendStatus(200)
     }
-    return 0
-  })
-  response.send(upcomingConferences)
-}))
+  }
+})
+
+app.get('/people/count', async (req, res) => {
+  try {
+    let response = null
+    response = await axios.get(`https://${process.env.NATIONBUILDER_SLUG}.nationbuilder.com/api/v1/people/count?access_token=${process.env.NATIONBUILDER_TOKEN}`, { headers: { Accept: 'application/json', 'Content-Type': 'application/json' }, validateStatus: () => true })
+    if (response) {
+      res.send({ count: response.data.people_count })
+    } else {
+      res.sendStatus(400)
+    }
+  } catch (ex) {
+    log.error(ex)
+    if (req.body.redirect) {
+      res.redirect(req.body.redirect)
+    } else {
+      res.sendStatus(200)
+    }
+  }
+})
+
+app.get('/conference-calls/upcoming', async (request, response) => {
+  try {
+    const name = (typeof request.query.name === 'undefined') ? '' : unescape(request.query.name)
+    const upcomingConferences = {
+      conferences: []
+    }
+
+    const upcomingConferenceIds = await maestro.getUpcomingConferences(name)
+    for (let index = 0; index < upcomingConferenceIds.length; index++) {
+      const conferenceData = await maestro.getConferenceData(upcomingConferenceIds[index])
+      const newMaestroConference = maestro.formattedData(conferenceData)
+      upcomingConferences.conferences.push(newMaestroConference)
+    }
+    upcomingConferences.conferences.sort((a, b) => {
+      if (a.timeInSeconds < b.timeInSeconds) {
+        return -1
+      }
+      if (a.timeInSeconds > b.timeInSeconds) {
+        return 1
+      }
+      return 0
+    })
+    response.send(upcomingConferences)
+  } catch (ex) {
+    log.error(ex)
+    if (request.body.redirect) {
+      response.redirect(request.body.redirect)
+    } else {
+      response.sendStatus(200)
+    }
+  }
+})
 
 app.listen(port, () => {
   log.info(`Node app is running on port ${port}`)
