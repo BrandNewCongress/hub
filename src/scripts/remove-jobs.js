257bc609d172b315d50a7171f22fecb8f8ce5d6e
saikat <saikat@gomockingbird.com>
2017-01-31 23:15:45 -0800

    Try again

--- src/scripts/remove-jobs.js
+++ src/scripts/remove-jobs.js
@@ -1,10 +1,10 @@
 var kue = require('kue')
 
-const queue = kue.createQueue({
+kue.redis.configureFactory({
   redis: process.env.REDIS_URL
 })
 console.log('Deleting jobs:', process.argv[2])
-queue.Job.rangeByState('complete', 0, process.argv[2], 'asc', (err, jobs) => {
+kue.Job.rangeByState('complete', 0, process.argv[2], 'asc', (err, jobs) => {
   jobs.forEach((job) => {
     job.remove(() => {
       console.log('removed ', job.id)
