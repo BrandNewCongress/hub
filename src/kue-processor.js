33698ce0e02547d2fff9c789778702d482fad7af
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2017-02-10 15:59:04 -0500

    added edit + get endpoints, get tested, edit not yet

--- src/kue-processor.js
+++ src/kue-processor.js
@@ -29,3 +29,14 @@ queue.process('createNomination', async (job, done) => {
   }
   return done()
 })
+
+queue.process('editPerson', async (job, done) => {
+  try {
+    const {personId, data} = job
+    const updated = await airtable.createOrUpdatePerson(personId, data)
+  } catch (ex) {
+    log.error(ex)
+    return done()
+  }
+  return done()
+}
