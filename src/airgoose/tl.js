6a9370a2109c60ac0af41800a6536c40600c6b1d
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2017-03-06 19:33:27 -0500

    compute metrics

--- src/airgoose/tl.js
+++ src/airgoose/tl.js
@@ -3,5 +3,11 @@ export default {
   Evaluation: 'Nominee Evaluations',
   Nomination: 'Nominations',
   District: 'Congressional Districts',
-  Address: 'Addresses'
+  Address: 'Addresses',
+  State: 'States',
+  DistrictData: 'District Data',
+  PhoneNumber: 'Phone Numbers',
+  Email: 'Emails',
+  Contact: 'Contact Logs',
+  Team: 'Teams'
 }
---
+++ src/scripts/metrics.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+const monk = require('monk')
+const toAirCase = require('../airgoose/to-air-case').default
+const db = monk(process.env.MONGODB_URI || 'localhost:27017/bnc')
+
+const statuses = {}
+
+console.log('hi')
+
+const People = db.get('People')
+People
+.find()
+.then(people => {
+  console.log('hello')
+
+  people.forEach(p =>
+    statuses[p.nominationStatus] = statuses[p.nominationStatus] ? statuses[p.nominationStatus] + 1 : 1
+  )
+
+  const cleaner = {}
+  Object
+    .keys(statuses)
+    .filter(s => s != 'Not Nominated')
+    .sort()
+    .forEach(s =>
+      cleaner[s] = statuses[s]
+    )
+
+  console.log(JSON.stringify(cleaner, null, 2))
+})
+.catch(err => {
+  console.log(err)
+})
