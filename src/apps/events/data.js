e23ee337da94ed8cf0bbf72102b983aa48473462
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2017-05-15 14:40:47 -0400

    shhhhh

--- src/apps/events/data.js
+++ src/apps/events/data.js
@@ -1,12 +1,22 @@
-const reverseMap = map => Object.keys(map).reduce(
-  (acc, key) =>
-    Object.assign(acc, { [map[key]]: key }),
-  {}
-)
+const reverseMap = map =>
+  Object.keys(map).reduce(
+    (acc, key) => Object.assign(acc, { [map[key]]: key }),
+    {}
+  )
 
 const slugToName = {
   coribush: 'Cori Bush',
   paulajean: 'Paula Jean Swearengin',
+  sarahsmith: 'Sarah Smith',
+  letitiaplummer: 'Letitia Plummer',
+  anthonyclark: 'Anthony Clark',
+  richardrice: 'Richard Rice',
+  demonddrummer: 'Demond Drummer',
+  robbryerse: 'Robb Ryerse',
+  michaelhephburn: 'Michael Hephburn',
+  chardorichardson: 'Chardo Richardon',
+  dannyellyson: 'Danny Ellyson',
+  ericterrell: 'Eric Terrell',
   brandnewcongress: 'General Brand New Congress'
 }
 
@@ -15,7 +25,17 @@ const nameToSlug = reverseMap(slugToName)
 const candidateToCalendar = {
   coribush: 6,
   paulajean: 7,
-  brandnewcongress: 9
+  brandnewcongress: 9,
+  dannyellyson: 10,
+  chardorichardson: 11,
+  michaelhephburn: 12,
+  robbryerse: 13,
+  demonddrummer: 14,
+  richardrice: 15,
+  anthonyclark: 16,
+  letitiaplummer: 17,
+  ericterrell: 18,
+  sarahsmith: 19
 }
 
 const calendarToCandidate = reverseMap(candidateToCalendar)
