96224503c122ae34866d408030dca209e7e7b1fc
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2017-05-18 14:15:23 -0400

    bug fix

--- src/scripts/repost-nominations.js
+++ src/scripts/repost-nominations.js
@@ -3,19 +3,25 @@ const db = monk(process.env.MONGODB_URI)
 const request = require('superagent')
 
 const query = {
-  path: '/nominations'
+  path: '/people',
+  $or: [
+    { 'data.candidate': 'michaelhepburn' },
+    { 'data.subscriptions': 'justicedemocrats' }
+  ]
 }
 
 db.get('API Logs').find(query).then(posts => {
   console.log(`Got ${posts.length} posts`)
   console.log(posts.slice(posts.length - 1))
+  process.exit()
 
   posts.slice(posts.length - 1).forEach(p => {
     request
-    .post('https://api.brandnewcongress.org/nominations')
-    .send(Object.assign(p.data, {forceSource: 'brandnewcongress.org'}))
-    .end((err, res) => {
-      console.log(`Success for ${JSON.stringify(p)}`)
-    })
+      .post('https://api.brandnewcongress.org/nominations')
+      .query({ dontLog: true })
+      .send(p.data)
+      .end((err, res) => {
+        console.log(`Success for ${JSON.stringify(p)}`)
+      })
   })
 })
