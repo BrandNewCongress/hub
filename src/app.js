12e855089eeabacb03318a6f4323412ea18f7d0e
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2017-05-28 18:05:08 -0400

    no more subscriptions to jd

--- src/app.js
+++ src/app.js
@@ -207,9 +207,6 @@ app.post('/people', apiLog, async (req, res) => {
     const signupSource = `${source(req)}`
     const tags = [`Source: ${signupSource}`]
 
-    if (body.subscriptions && body.subscriptions.match('justicedemocrats'))
-      tags.push('Source: Justice Democrats')
-
     const createJob = queue.createJob('createPerson', {
       name: stripBadPunc(body.fullName),
       email: body.email,
