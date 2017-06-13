bfaee85370f1e2e8b28846f25a18bb0ede96085c
saikat <saikat@gomockingbird.com>
2017-06-13 14:11:35 -0400

    Change legacy taggings a bit and also nominator tag

--- src/scripts/nb-to-bsd.js
+++ src/scripts/nb-to-bsd.js
@@ -2,12 +2,12 @@ const bsdConstructor = require('../bsd')
 const nationbuilder = require('../nationbuilder') 
 const moment = require('moment')
 const log = require('../log')
+const { convertTags } = require('../legacy-tag-map')
+const Baby = require('babyparse')
 
 const bsd = new bsdConstructor(process.env.BSD_HOST, process.env.BSD_ID, process.env.BSD_SECRET)
-const REFRESH_CONS_GROUPS = false
 
 async function nbPersonToBSDCons(person) {
-  // TODO fix this to use blacklist
   consGroups = consGroups.map((group) => consGroupIdMap[group])
     .map((group) => ({ id: group }))
   if (person.email && consGroups.length > 0) {
@@ -75,44 +75,49 @@ async function nbPersonToBSDCons(person) {
 async function syncPeople() {
   let tagResults = await nationbuilder.makeRequest('GET', 'tags', { params: { limit: 100 } })
   let allTags = []
-  while (tagResults.data.next) {
+  const tagPrefixWhitelist = [
+    'Signup',
+    'Donor',
+    'Action',
+    'Available',
+    'Skill',
+    'Petition'
+  ]
+  while (true) {
     log.info('Getting tags...')
     const tags = tagResults.data.results
-    allTags = allTags.concat(tags.map((tag) => tag.name))
-    const next = tagResults.data.next.split('?')    
-    tagResults = await nationbuilder.makeRequest('GET', tagResults.data.next, { params: { limit: 100 } })
-  }
-  const notFoundTags = []
-  allTags.forEach((tag) => {
-    let found = false
-    Object.keys(TagMap).forEach((currentTag) => {
-      if (currentTag === tag) {
-        found = true
-      }
+    tags.forEach((tag) => {
+      tagPrefixWhitelist.forEach((prefix) => {
+        if (tag.name.indexOf(prefix) === 0) {
+          allTags.push(tag.name)
+        }
+      })
     })
-    if (found === false) {
-      throw new Error(`TAG NOT FOUND, CANNOT SYNC: ${tag}`)
-    }
-  })
 
-  if (REFRESH_CONS_GROUPS) {
-    let allConsGroups = Object.keys(TagMap).map((tag) => TagMap[tag])
-      .filter((ele) => ele !== null)
-    allConsGroups = allConsGroups.filter((ele, pos) => allConsGroups.indexOf(ele) === pos)
-    const groupsToCreate = []
-    const groups = {}
-    for (let index = 0; index < allConsGroups.length; index++) {
-      const group = await bsd.getConstituentGroupByName(allConsGroups[index])
-      if (group === null) {
-        groupsToCreate.push(allConsGroups[index])
-      } else {
-        groups[group.name] = group.cons_group_id
-      }
+    if (tagResults.data.next) {
+      const next = tagResults.data.next.split('?')    
+      tagResults = await nationbuilder.makeRequest('GET', tagResults.data.next, { params: { limit: 100 } })
+    } else {
+      break
     }
-    for (let index = 0; index < groupsToCreate.length; index++) {
-      const group = groupsToCreate[index]
-      await bsd.createConstituentGroups([group])
+  }
+
+  let allConsGroups = Object.keys(TagMap).map((tag) => TagMap[tag])
+    .filter((ele) => ele !== null)
+  allConsGroups = allConsGroups.filter((ele, pos) => allConsGroups.indexOf(ele) === pos)
+  const groupsToCreate = []
+  const groups = {}
+  for (let index = 0; index < allConsGroups.length; index++) {
+    const group = await bsd.getConstituentGroupByName(allConsGroups[index])
+    if (group === null) {
+      groupsToCreate.push(allConsGroups[index])
+    } else {
+      groups[group.name] = group.cons_group_id
     }
+  }
+  for (let index = 0; index < groupsToCreate.length; index++) {
+    const group = groupsToCreate[index]
+    await bsd.createConstituentGroups([group])
   }
 
   // SYNC PEOPLE
@@ -251,148 +256,97 @@ async function syncEvents() {
   }
   // DELETE BSD EVENTS TODO
 }
-async function sync() {
-  await syncEvents()
+
+function deletePress() {
+  const peopleToDelete = []
+  const pressCSV = Baby.parseFiles('/Users/saikat/Development/Source/brandnewcongress/hub/src/press_list.csv', {
+    header: true
+  })
+
+  const people = pressCSV.data
+  for (let index = 0; index < people.length; index++) {
+    person = people[index]
+    const tags = person.tag_list.split(',')
+    const newTags = convertTags(tags)
+    if (newTags.length === 0) {
+      peopleToDelete.push(person)
+    }
+  }
+  const deleteCSV = Baby.unparse(peopleToDelete, {
+    header: true
+  })
+  console.log(deleteCSV)
 }
 
-const consGroupIdMap = { 
-  'Signup: Alexandria Ocasio-Cortez': '14',
-  'Available: 0-5 Hours': '33',
-  'Available: 10-20 Hours': '34',
-  'Available: 20-30 Hours': '35',
-  'Available: 30+ Hours': '36',
-  'Available: 5-10 Hours': '37',
-  'Action: Attended CC: Nomination': '27',
-  'Action: Attended CC: Orientation': '38',
-  'Action: Attended CC: Research': '39',
-  'Action: Attended CC: Social Media': '40',
-  'Action: Attended CC: State of BNC': '41',
-  'Action: Attended CC: Help Desk': '42',
-  'Action: Attended CC: Event Host': '43',
-  'Skill: Programming': '44',
-  'Skill: Video': '45',
-  'Skill: Data Entry': '46',
-  'Action: Attended Event: 2016/08/24 @ Oakland, CA': '47',
-  'Action: Attended Event: 2016/07/22 @ Olympia, WA': '48',
-  'Action: Attended Event: 2016/07/11 @ Omaha, NE': '49',
-  'Action: Attended Event: 2016/06/29 @ Orlando, FL': '50',
-  'Action: Attended Event: 2016/07/11 @ Overland Park, KS': '51',
-  'Action: Attended Event: 2016/07/23 @ Philadelphia, PA': '52',
-  'Action: Attended Event: 2016/08/20 @ Pittsburgh, PA': '53',
-  'Action: Attended Event: 2016/08/14 @ Providence, RI': '54',
-  'Action: Attended Event: 2016/06/04 @ Raleigh, NC': '55',
-  'Action: Attended Event: 2016/08/11 @ Rochester, NY': '56',
-  'Action: Attended Event: 2016/08/25 @ Sacramento, CA': '57',
-  'Action: Attended Event: 2016/07/24 @ Salt Lake City, UT': '58',
-  'Action: Attended Event: 2016/07/01 @ San Antonio, TX': '59',
-  'Action: Attended Event: 2016/06/14 @ San Bernardino, CA': '60',
-  'Action: Attended Event: 2016/08/26 @ San Diego, CA': '61',
-  'Action: Attended Event: 2016/08/23 @ San Francisco, CA': '62',
-  'Action: Attended Event: 2016/08/22 @ San Jose, CA': '63',
-  'Action: Attended Event: 2016/07/19 @ Santa Ana, CA': '64',
-  'Action: Attended Event: 2016/08/27 @ Santa Barbara, CA': '65',
-  'Action: Attended Event: 2016/07/11 @ Savannah, GA': '66',
-  'Action: Attended Event: 2016/07/20 @ Seattle, WA': '67',
-  'Action: Attended Event: 2016/07/12 @ Sioux Falls, SD': '68',
-  'Action: Attended Event: 2016/08/12 @ Spokane, WA': '69',
-  'Action: Attended Event: 2016/06/26 @ Stamford, CT': '70',
-  'Action: Attended Event: 2016/08/09 @ Syracuse, NY': '71',
-  'Action: Attended Event: 2016/07/21 @ Tacoma, WA': '72',
-  'Action: Attended Event: 2016/06/28 @ Tampa, FL': '73',
-  'Action: Attended Event: 2016/06/28 @ Dallas, TX': '74',
-  'Action: Attended Event': '75',
-  'Skill: Event Host': '76',
-  'Action: Hosted Event': '77',
-  'Action: Attended Event: 2016/07/30 @ Washington, DC': '78',
-  'Action: Attended Event: 2016/08/06 @ Worcester, MA': '79',
-  'Availability: Week Days': '80',
-  'Availability: Weekends': '81',
-  'Availability: Week Nights': '82',
-  'Skill: Graphic Design': '83',
-  'Skill: Help Desk': '84',
-  'Skill: Start Local Group': '85',
-  'Petition: Muslim Ban': '86',
-  'Skill: Nationbuilder': '87',
-  'Action: Nominated Candidate': '88',
-  'Candidate Nominee': '89',
-  'Skill: Photography': '90',
-  'Potential Leader': '91',
-  'Skill: Printing': '92',
-  'Skill: Process Engineer': '93',
-  'Skill: Research': '94',
-  'Signup: Ryan Stone': '20',
-  'Petition: Draft Cori Bush': '95',
-  'Petition: Kick out Joe Manchin': '96',
-  'Petition: Letter to Keith Ellison': '97',
-  'Petition: Medicare for All': '98',
-  'Petition: Syrian War': '99',
-  'Skill: Calling': '100',
-  'Skill: Field Outreach': '101',
-  'Skill: Flyering': '102',
-  'Skill: Door Knocking': '103',
-  'Skill: HR': '104',
-  'Skill: Legal': '105',
-  'Skill: Community Management': '106',
-  'Skill; Nationbuilder': '107',
-  'Skill: Office Help': '108',
-  'Skill: Press': '109',
-  'Skill: Texting': '110',
-  'Skill: Social Media Sharing': '111',
-  'Skill: Speaker': '112',
-  'Skill: Supporter Housing': '113',
-  'Skill: Travel Management': '114',
-  'Skill: Event Venue': '115',
-  'Skill: Web Design': '116',
-  'Skill: Writing': '117',
-  'Skill: Social Media': '118',
-  'Signup: Adrienne Bell': '17',
-  'Donor: Adrienne Bell': '119',
-  'Donor: Alexandria Ocasio-Cortez': '120',
-  'Signup: Anthony Clark': '18',
-  'Donor: Anthony Clark': '121',
-  'Signup: Brand New Congress': '11',
-  'Donor: Brand New Congress': '122',
-  'Signup: Chardo Richardson': '23',
-  'Donor: Chardo Richardson': '123',
-  'Signup: Cori Bush': '12',
-  'Donor: Cori Bush': '124',
-  'Signup: Danny Ellyson': '21',
-  'Donor: Danny Ellyson': '125',
-  'Signup: Demond Drummer': '126',
-  'Donor: Demond Drummer': '127',
-  'Signup: Eric Terrell': '128',
-  'Donor: Eric Terrell': '129',
-  'Source: Facebook': '130',
-  'Signup: Hector Morales': '24',
-  'Donor: Hector Morales': '131',
-  'Source: Incoming Call': '132',
-  'Signup: Justice Democrats': '10',
-  'Donor: Justice Democrats': '133',
-  'Signup: Letitia Plummer': '22',
-  'Donor: Letitia Plummer': '134',
-  'Signup: Michael Hepburn': '135',
-  'Donor: Michael Hepburn': '136',
-  'Signup: Paula Jean Swearengin': '13',
-  'Donor: Paula Jean Swearengin': '137',
-  'Signup: Paul Perry': '25',
-  'Donor: Paul Perry': '138',
-  'Signup: Richard Rice': '26',
-  'Donor: Richard Rice': '139',
-  'Signup: Robb Ryerse': '15',
-  'Donor: Robb Ryerse': '140',
-  'Donor: Ryan Stone': '141',
-  'Signup: Sarah Smith': '19',
-  'Donor: Sarah Smith': '142',
-  'Signup: Tamarah Begay': '143',
-  'Donor: Tamarah Begay': '144',
-  'Source: Incoming Text Message': '145',
-  'Source: Twitter': '146',
-  'Action: Attended Event: 4/30/2017 @ St. Louis, MO': '32',
-  'Action: Attended Event: 2017/05/06 @ St. Louis, MO': '31',
-  'Action: Attended Event: 2017/05/07 @ St. Louis, MO': '30',
-  'Action: Attended Event: 2017/05/13 @ St. Louis, MO': '29',
-  'Action: Attended Event: 2017/05/03 @ St. Louis, MO': '28' 
+async function cleanUpNBTags() {
+  let tagResults = await nationbuilder.makeRequest('GET', 'tags', { params: { limit: 100 } })
+  let allTags = []
+  while (true) {
+    log.info('Getting tags...')
+    const tags = tagResults.data.results
+    allTags = allTags.concat(tags.map((tag) => tag.name))
+    if (tagResults.data.next) {
+      const next = tagResults.data.next.split('?')    
+      tagResults = await nationbuilder.makeRequest('GET', tagResults.data.next, { params: { limit: 100 } })
+    } else {
+      break
+    }
+  }
+  let count = 0
+  for (let index = 0; index < allTags.length; index++) {
+    console.log(`Processing ${allTags[index]}...`)
+    const tag = allTags[index]
+    if (tag.indexOf('Donor: ') === 0 || 
+      tag.indexOf('Signup: ') === 0 || 
+      tag.indexOf('Action: ') === 0 || 
+      tag.indexOf('Petition: ') === 0 || 
+      tag.indexOf('Skill: ') === 0 || 
+      tag.indexOf('Available: ') === 0 || 
+      tag.indexOf('Field: ') === 0 ||
+      tag === 'Supporter' ||
+      tag === 'movoters:MO-Federal-1 - 04/20/2017 14:15:34:439') {
+      continue
+    }
+    let peopleResults = await nationbuilder.makeRequest('GET', `tags/${tag}/people`, { params: { limit: 100 } })
+    while (true) {
+      const people = peopleResults.data.results
+      for (let pIndex = 0; pIndex < people.length; pIndex++) {
+        const person = people[pIndex]
+        const newTags = convertTags(person.tags)
+        await nationbuilder.makeRequest('DELETE', `people/${person.id}/taggings`, {
+          body: {
+            tagging: {
+              tag: person.tags
+            }
+          }
+        })
+        await nationbuilder.makeRequest('PUT', `people/${person.id}/taggings`, { 
+          body: { 
+            tagging: {
+              tag: newTags
+            }
+          }
+        })
+        console.log(person.email)
+      }
+
+      if (peopleResults.data.next) {
+        const next = peopleResults.data.next.split('?')
+        peopleResults = await nationbuilder.makeRequest('GET', peopleResults.data.next, { params: { limit: 100 } })
+      } else {
+        break
+      }
+    }
+    console.log(tag)
+    console.log(count)
+    count = count + 1
+  }
 }
+
+async function sync() {
+  await cleanUpNBTags()
+}
+
 
 const timezoneMap = {
   'Eastern Time (US & Canada)' : 'US/Eastern',
