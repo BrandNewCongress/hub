b90bef6dfd185ec8343de9fce9b99f0ac7253095
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2017-08-14 15:28:30 -0400

    no broken links

--- src/apps/events/format.js
+++ src/apps/events/format.js
@@ -12,7 +12,7 @@ module.exports = {
   event: e => {
     return {
       id: e.id,
-      url: `http://now.brandnewcongress.org/events/${e.path}`,
+      url: `http://now.brandnewcongress.org/events${e.path}`,
       title: e.headline,
       intro: e.intro,
       startTime: new Date(e.start_time).toISOString(),
